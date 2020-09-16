a038db02c105742898c1d3f9b2ab956a203de8fe
Brett Horner <hornerbrett@gmail.com>
2020-09-16 15:27:53 -0700

    plugins

--- wp-content/plugins/woocommerce/vendor/autoload.php
+++ wp-content/plugins/woocommerce/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit59af27a7bf321fa3c5eec815bc33891c::getLoader();
+return ComposerAutoloaderInit1e69adc4e12f3a0de649acee72d963eb::getLoader();
