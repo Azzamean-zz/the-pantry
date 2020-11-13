3dac5b8c159b7e3eed0ff7e9ec1147e1a13d6dec
Brett Horner <hornerbrett@gmail.com>
2020-11-13 14:14:16 -0800

    header

--- wp-content/themes/storefront-child-theme-master/header.php
+++ wp-content/themes/storefront-child-theme-master/header.php
@@ -10,6 +10,9 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+
+<meta http-equiv="Cache-control" content="no-cache">
+
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
 <link rel="profile" href="http://gmpg.org/xfn/11">
