28ffee4425b0174144d2cd1b4e032961996ea782
Patrick Jackson <pjackson@goldenpathsolutions.com>
2021-05-17 19:46:27 -0700

    add plugin stub

---
+++ wp-content/plugins/the-pantry-attendee-manager/the-pantry-attendee-manager.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:       The Pantry Attendee Manager
+ * Description:       Adds customizations to better integrate attendee management between plugins, and The Pantry's workflow
+ * Version:           1.0.0
+ * Author:            New Theory
+ * Author URI:        https://newtheory.is/
+ * Text Domain:       the-pantry
+ */
